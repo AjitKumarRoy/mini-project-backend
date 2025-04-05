b562d9f0ae2ed0d94d369234f913060ace5ab477
AjitKumarRoy <ak47riderji@gmail.com>
2025-04-05 23:04:34 +0530

    remove extra cors

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,12 @@
 const { verifyToken } = require('../utils/jwt');
 
 const authMiddleware = (req, res, next) => {
+    const tokenFromCookie = req.cookies.token;
+    const tokenFromHeader = req.headers.authorization?.split(' ')[1];
+    console.log('Token from cookie:', tokenFromCookie);
+    console.log('Token from header:', tokenFromHeader);
+
+    const token = tokenFromCookie || tokenFromHeader;
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
     if (!token) {
         return res.status(401).json({ 'message': 'Access denied. No token provided.' });
