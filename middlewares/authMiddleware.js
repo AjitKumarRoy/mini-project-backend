80f652b9d618270604d9946d606021255a810846
AjitKumarRoy <ak47riderji@gmail.com>
2025-04-05 23:28:57 +0530

    added extra cors

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,8 +3,8 @@ const { verifyToken } = require('../utils/jwt');
 const authMiddleware = (req, res, next) => {
     const tokenFromCookie = req.cookies.token;
     const tokenFromHeader = req.headers.authorization?.split(' ')[1];
-    console.log('Token from cookie:', tokenFromCookie);
-    console.log('Token from header:', tokenFromHeader);
+    // console.log('Token from cookie:', tokenFromCookie);
+    // console.log('Token from header:', tokenFromHeader);
 
     const token = tokenFromCookie || tokenFromHeader;
     // const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
